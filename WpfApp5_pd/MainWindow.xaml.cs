da2d76a5fc7476fb6beaeffc9ccb798c4e7799dd
denisfilonov <dean.mitz6@gmail.com>
2023-01-10 14:59:37 +0100

    Add project files.

---
+++ WpfApp5_pd/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp5_pd
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Product> Products { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Random random = new Random();
+            //Products = new List<Product>
+            //{
+            //    new Product{Name="Шоколад", Count=2,Price = 17,Code = random.Next(999999)},
+            //    new Product{Name="Какао", Count=1,Price = 25,Code = random.Next(999999)},
+            //    new Product{Name="Молоко", Count=2,Price = 18,Code = random.Next(999999)},
+            //    new Product{Name="Мандарины", Count=3,Price = 45,Code = random.Next(999999)},
+            //    new Product{Name="Апельсиновый сок", Count=1,Price = 23,Code = random.Next(999999)},
+            //    new Product{Name="Зефир", Count=1,Price = 32,Code = random.Next(999999)},
+            //    new Product{Name="Бананы", Count=4,Price = 28,Code = random.Next(999999)},
+            //    new Product{Name="Консервированные персики", Count=3,Price = 28,Code = random.Next(999999)}
+            //};
+            Load();
+
+
+            //foreach (var item in Products)
+            //{
+            //    lbProducts.Items.Add(item.Name);
+            //}
+        }
+
+        private void lbProducts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Save();
+        }
+
+        private void Save()
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            try
+            {
+                using (Stream fStream = File.Create("Data.bin"))
+                {
+                    bf.Serialize(fStream, Products);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Data save error!");
+            }
+        }
+
+        private void Load()
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            try
+            {
+                using (var fr = new FileStream("Data.bin", FileMode.Open))
+                {
+                    Products = bf.Deserialize(fr) as List<Product>;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ WpfApp5_pd/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WpfApp5_pd
+{
+    [Serializable]
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Count { get; set; }
+        public double Price { get; set; }
+        public double RealPrice { get; set; }
+        public bool Purchased { get; set; }
+
+        //[NonSerialized]
+        public int Code { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
